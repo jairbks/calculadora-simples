22af00d541dc0607059b211d06b9dbe295a717c4
jairbks <Jairccb2324@gmail.com>
2025-07-26 23:09:28 -0300

    minha-calculadora-simples
    Adiciona código da calculadora e script shell

---
+++ Calculadora.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Calculadora simples"
+echo "Digite o primeiro número:"
+read num1
+echo "Digite o segundo número:"
+read num2
+soma=$((num1 + num2))
+echo "Resultado da soma: $soma"
+
+ 
---
+++ README.md
@@ -0,0 +1,12 @@
+# logica programação
+curso de logica de programação em python
+
+
+## Aulas
+- Aula 1
+- Aula 2
+- Aula 3
+- Aula 4
+- Aula 5
+
+
